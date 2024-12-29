8ae0a07f19cf990f8adf2e143c3643132141eb0c
zmilla93 <zmilla93@gmail.com>
2024-12-28 22:16:24 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                Hello world!
+            </h1>
+            <div>
+                This website was made by:
+                <ul>
+                    <li>Generate a new project using Webpack</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default App
